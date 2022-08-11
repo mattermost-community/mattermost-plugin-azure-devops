9f65a10331b121993ed775abfe5a9b5fb85c263e
Abhishek Verma <abhishek.verma@brightscout.com>
2022-08-11 11:27:24 +0530

    [MI-1986]: Create plugin API to fetch linked projects list

--- server/constants/routes.go
+++ server/constants/routes.go
@@ -2,8 +2,9 @@ package constants
 
 const (
 	// Plugin API Routes
-	APIPrefix         = "/api/v1"
-	WildRoute         = "{anything:.*}"
-	PathOAuthConnect  = "/oauth/connect"
-	PathOAuthCallback = "/oauth/complete"
+	APIPrefix                = "/api/v1"
+	WildRoute                = "{anything:.*}"
+	PathOAuthConnect         = "/oauth/connect"
+	PathOAuthCallback        = "/oauth/complete"
+	PathGetAllLinkedProjects = "/link/project"
 )

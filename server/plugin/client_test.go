a15cf4fcea88a1f820535fc0c9a36e60384db2d3
ayusht2810 <ayush.thakur@joshtechnologygroup.com>
2022-08-23 18:02:46 +0530

    [MI-2074] Add test cases for client and command

---
+++ server/plugin/client_test.go
@@ -0,0 +1,308 @@
+package plugin
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"reflect"
+	"testing"
+
+	"bou.ke/monkey"
+	"github.com/Brightscout/mattermost-plugin-azure-devops/server/serializers"
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClientGenerateOAuthToken(t *testing.T) {
+	defer monkey.UnpatchAll()
+	p := Plugin{}
+	mockAPI := &plugintest.API{}
+	p.API = mockAPI
+	c := InitClient(&p)
+	p.Client = c
+	for _, testCase := range []struct {
+		description string
+		err         error
+		statusCode  int
+	}{
+		{
+			description: "test ClientGenerateOAuthToken",
+			err:         nil,
+			statusCode:  http.StatusOK,
+		},
+		{
+			description: "test ClientGenerateOAuthToken with error",
+			err:         errors.New("mock-error"),
+			statusCode:  http.StatusInternalServerError,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(c), "Call", func(_ *client, basePath, method, path, contentType, mattermostUserID string, inBody io.Reader, out interface{}, formValues url.Values) (responseData []byte, statusCode int, err error) {
+				return nil, testCase.statusCode, testCase.err
+			})
+
+			_, statusCode, err := p.Client.GenerateOAuthToken(mockAPI.TestData().URLValues())
+
+			if testCase.err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.statusCode, statusCode)
+
+		})
+	}
+}
+
+func TestCreateTask(t *testing.T) {
+	defer monkey.UnpatchAll()
+	p := Plugin{}
+	mockAPI := &plugintest.API{}
+	p.API = mockAPI
+	c := InitClient(&p)
+	p.Client = c
+	for _, testCase := range []struct {
+		description string
+		err         error
+		statusCode  int
+	}{
+		{
+			description: "test CreateTask",
+			err:         nil,
+			statusCode:  http.StatusOK,
+		},
+		{
+			description: "test CreateTask with error",
+			err:         errors.New("mock-error"),
+			statusCode:  http.StatusInternalServerError,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(c), "Call", func(_ *client, basePath, method, path, contentType, mattermostUserID string, inBody io.Reader, out interface{}, formValues url.Values) (responseData []byte, statusCode int, err error) {
+				return nil, testCase.statusCode, testCase.err
+			})
+
+			_, statusCode, err := p.Client.CreateTask(&serializers.CreateTaskRequestPayload{
+				Fields: serializers.CreateTaskFieldValue{
+					Description: "mockDescription",
+				},
+			}, "mockMattermostUSerID")
+
+			if testCase.err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.statusCode, statusCode)
+
+		})
+	}
+}
+
+func TestGetTask(t *testing.T) {
+	defer monkey.UnpatchAll()
+	p := Plugin{}
+	mockAPI := &plugintest.API{}
+	p.API = mockAPI
+	c := InitClient(&p)
+	p.Client = c
+	for _, testCase := range []struct {
+		description string
+		err         error
+		statusCode  int
+	}{
+		{
+			description: "test GetTask",
+			err:         nil,
+			statusCode:  http.StatusOK,
+		},
+		{
+			description: "test GetTask with error",
+			err:         errors.New("mock-error"),
+			statusCode:  http.StatusInternalServerError,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(c), "Call", func(_ *client, basePath, method, path, contentType, mattermostUserID string, inBody io.Reader, out interface{}, formValues url.Values) (responseData []byte, statusCode int, err error) {
+				return nil, testCase.statusCode, testCase.err
+			})
+
+			_, statusCode, err := p.Client.GetTask("mockOrganization", "mockTaskID", "mockMattermostUSerID")
+
+			if testCase.err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.statusCode, statusCode)
+
+		})
+	}
+}
+
+func TestLink(t *testing.T) {
+	defer monkey.UnpatchAll()
+	p := Plugin{}
+	mockAPI := &plugintest.API{}
+	p.API = mockAPI
+	c := InitClient(&p)
+	p.Client = c
+	for _, testCase := range []struct {
+		description string
+		err         error
+		statusCode  int
+	}{
+		{
+			description: "test Link",
+			err:         nil,
+			statusCode:  http.StatusOK,
+		},
+		{
+			description: "test Link with error",
+			err:         errors.New("mock-error"),
+			statusCode:  http.StatusInternalServerError,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(c), "Call", func(_ *client, basePath, method, path, contentType, mattermostUserID string, inBody io.Reader, out interface{}, formValues url.Values) (responseData []byte, statusCode int, err error) {
+				return nil, testCase.statusCode, testCase.err
+			})
+
+			_, statusCode, err := p.Client.Link(&serializers.LinkRequestPayload{}, "mockMattermostUSerID")
+
+			if testCase.err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.statusCode, statusCode)
+
+		})
+	}
+}
+
+func TestCreateSubscription(t *testing.T) {
+	defer monkey.UnpatchAll()
+	p := Plugin{}
+	mockAPI := &plugintest.API{}
+	p.API = mockAPI
+	c := InitClient(&p)
+	p.Client = c
+	for _, testCase := range []struct {
+		description string
+		err         error
+		statusCode  int
+	}{
+		{
+			description: "test CreateSubscription",
+			err:         nil,
+			statusCode:  http.StatusOK,
+		},
+		{
+			description: "test CreateSubscription with error",
+			err:         errors.New("mock-error"),
+			statusCode:  http.StatusInternalServerError,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(c), "Call", func(_ *client, basePath, method, path, contentType, mattermostUserID string, inBody io.Reader, out interface{}, formValues url.Values) (responseData []byte, statusCode int, err error) {
+				return nil, testCase.statusCode, testCase.err
+			})
+
+			_, statusCode, err := p.Client.CreateSubscription(&serializers.CreateSubscriptionRequestPayload{}, &serializers.ProjectDetails{}, "mockChannelID", "mockPluginURL", "mockMattermostUSerID")
+
+			if testCase.err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.statusCode, statusCode)
+
+		})
+	}
+}
+
+func TestDeleteSubscription(t *testing.T) {
+	defer monkey.UnpatchAll()
+	p := Plugin{}
+	mockAPI := &plugintest.API{}
+	p.API = mockAPI
+	c := InitClient(&p)
+	p.Client = c
+	for _, testCase := range []struct {
+		description string
+		err         error
+		statusCode  int
+	}{
+		{
+			description: "test DeleteSubscription",
+			err:         nil,
+			statusCode:  http.StatusOK,
+		},
+		{
+			description: "test DeleteSubscription with error",
+			err:         errors.New("mock-error"),
+			statusCode:  http.StatusInternalServerError,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(c), "Call", func(_ *client, basePath, method, path, contentType, mattermostUserID string, inBody io.Reader, out interface{}, formValues url.Values) (responseData []byte, statusCode int, err error) {
+				return nil, testCase.statusCode, testCase.err
+			})
+
+			statusCode, err := p.Client.DeleteSubscription("mockOrganization", "mockSubscriptionID", "mockMattermostUSerID")
+
+			if testCase.err != nil {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, testCase.statusCode, statusCode)
+
+		})
+	}
+}
+
+func TestCall(t *testing.T) {
+	p := Plugin{}
+	c := InitClient(&p)
+	p.Client = c
+	for _, testCase := range []struct {
+		description string
+	}{
+		{
+			description: "test Call",
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(&p), "AddAuthorization", func(_ *Plugin, _ *http.Request, _ string) error {
+				return nil
+			})
+			monkey.PatchInstanceMethod(reflect.TypeOf(&p), "RefreshOAuthToken", func(_ *Plugin, _ string) error {
+				return nil
+			})
+			client := &client{
+				plugin:     &p,
+				httpClient: &http.Client{},
+			}
+
+			_, _, err := client.Call("mockBasePath", "mockMethod", "mockPath", "mockContentType", "mockMattermostUserID", nil, nil, url.Values{})
+			assert.Error(t, err)
+
+		})
+	}
+}
---
+++ server/plugin/command_test.go
@@ -0,0 +1,115 @@
+package plugin
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"bou.ke/monkey"
+	"github.com/Brightscout/mattermost-plugin-azure-devops/mocks"
+	"github.com/Brightscout/mattermost-plugin-azure-devops/server/constants"
+	"github.com/golang/mock/gomock"
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestExecuteCommand(t *testing.T) {
+	p := Plugin{}
+	mockAPI := &plugintest.API{}
+	mockCtrl := gomock.NewController(t)
+	mockedStore := mocks.NewMockKVStore(mockCtrl)
+	p.API = mockAPI
+	p.Store = mockedStore
+	for _, testCase := range []struct {
+		description      string
+		commandArgs      *model.CommandArgs
+		ephemeralMessage string
+		isConnected      bool
+		patchAPICalls    func()
+	}{
+		{
+			description:      "empty command",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops"},
+			ephemeralMessage: constants.InvalidCommand,
+		},
+		{
+			description:      "help command",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops help"},
+			ephemeralMessage: constants.HelpText,
+		},
+		{
+			description:      "connect command",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops connect"},
+			ephemeralMessage: fmt.Sprintf(constants.ConnectAccount, p.GetPluginURLPath(), constants.PathOAuthConnect),
+		},
+		{
+			description:      "connect command with user already connected",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops connect"},
+			isConnected:      true,
+			ephemeralMessage: constants.UserAlreadyConnected,
+		},
+		{
+			description:      "disconnect command with user not connected",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops disconnect"},
+			ephemeralMessage: fmt.Sprintf(constants.ConnectAccountFirst, fmt.Sprintf(constants.ConnectAccount, p.GetPluginURLPath(), constants.PathOAuthConnect)),
+		},
+		{
+			description:      "disconnect command with user connected",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops disconnect", UserId: "mockUserID"},
+			isConnected:      true,
+			ephemeralMessage: constants.UserDisconnected,
+		},
+		{
+			description:      "link command",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops link"},
+			ephemeralMessage: fmt.Sprintf(constants.ConnectAccountFirst, fmt.Sprintf(constants.ConnectAccount, p.GetPluginURLPath(), constants.PathOAuthConnect)),
+		},
+		{
+			description:      "subscribe command",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops subscribe"},
+			ephemeralMessage: fmt.Sprintf(constants.ConnectAccountFirst, fmt.Sprintf(constants.ConnectAccount, p.GetPluginURLPath(), constants.PathOAuthConnect)),
+		},
+		{
+			description:      "boards create command",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops boards create"},
+			ephemeralMessage: fmt.Sprintf(constants.ConnectAccountFirst, fmt.Sprintf(constants.ConnectAccount, p.GetPluginURLPath(), constants.PathOAuthConnect)),
+		},
+		{
+			description:      "invalid command",
+			commandArgs:      &model.CommandArgs{Command: "/azuredevops abc"},
+			ephemeralMessage: constants.InvalidCommand,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+			mockAPI.On("SendEphemeralPost", mock.AnythingOfType("string"), mock.AnythingOfType("*model.Post")).Run(func(args mock.Arguments) {
+				post := args.Get(1).(*model.Post)
+				assert.Equal(t, testCase.ephemeralMessage, post.Message)
+			}).Once().Return(&model.Post{})
+
+			mockAPI.On("GetBundlePath").Return("/test-path", nil)
+			mockAPI.On("LogError", mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string"))
+			mockAPI.On("PublishWebSocketEvent", mock.AnythingOfType("string"), mock.Anything, mock.AnythingOfType("*model.WebsocketBroadcast")).Return(nil)
+
+			monkey.PatchInstanceMethod(reflect.TypeOf(&p), "UserAlreadyConnected", func(_ *Plugin, _ string) bool {
+				return testCase.isConnected
+			})
+
+			if testCase.ephemeralMessage == constants.UserDisconnected {
+				mockedStore.EXPECT().DeleteUser("mockUserID").Return(true, nil)
+			}
+
+			_, err := p.getCommand()
+			assert.NotNil(t, err)
+
+			response := p.getAutoCompleteData()
+			assert.NotNil(t, response)
+
+			res, err := p.ExecuteCommand(&plugin.Context{}, testCase.commandArgs)
+			assert.Nil(t, err)
+			assert.NotNil(t, res)
+		})
+	}
+}

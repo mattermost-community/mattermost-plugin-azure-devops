c593bbb056b7291979ed94839a2fe9ce6d27e5a1
Abhishek Verma <72438220+avas27JTG@users.noreply.github.com>
2022-09-06 13:29:24 +0530

    [MI-2030] Add filter to fetch subscriptions related to project (#68)
    Co-authored-by: Abhishek Verma <abhishek.verma@brightscout.com>

--- server/plugin/utils.go
+++ server/plugin/utils.go
@@ -170,10 +170,10 @@ func (p *Plugin) IsProjectLinked(projectList []serializers.ProjectDetails, proje
 	return nil, false
 }
 
-func (p *Plugin) IsSubscriptionPresent(subscriptionList []serializers.SubscriptionDetails, subscription serializers.SubscriptionDetails) (*serializers.SubscriptionDetails, bool) {
+func (p *Plugin) IsSubscriptionPresent(subscriptionList []*serializers.SubscriptionDetails, subscription *serializers.SubscriptionDetails) (*serializers.SubscriptionDetails, bool) {
 	for _, a := range subscriptionList {
 		if a.ProjectName == subscription.ProjectName && a.OrganizationName == subscription.OrganizationName && a.ChannelID == subscription.ChannelID && a.EventType == subscription.EventType {
-			return &a, true
+			return a, true
 		}
 	}
 	return nil, false

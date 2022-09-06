2beba52dfc7db50c0854557a14455cd2aecc40a0
Abhishek Verma <abhishek.verma@brightscout.com>
2022-09-06 19:08:59 +0530

    [MI-2098]: Fix CI

--- server/store/subscriptions.go
+++ server/store/subscriptions.go
@@ -106,8 +106,8 @@ func (s *Store) GetAllSubscriptions(userID string) ([]*serializers.SubscriptionD
 
 	var subscriptionList []*serializers.SubscriptionDetails
 	for _, subscription := range subscriptions.ByMattermostUserID[userID] {
-		subscription := &subscription
-		subscriptionList = append(subscriptionList, subscription)
+		subscription := subscription
+		subscriptionList = append(subscriptionList, &subscription)
 	}
 
 	return subscriptionList, nil

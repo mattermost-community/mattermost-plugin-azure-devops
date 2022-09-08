fe2cabc339c5be068d47184e9422bc78be209a89
Abhishek Verma <abhishek.verma@brightscout.com>
2022-09-08 11:29:30 +0530

    [MI-2109]: Review fix

--- webapp/src/containers/Rhs/projectList/index.tsx
+++ webapp/src/containers/Rhs/projectList/index.tsx
@@ -66,7 +66,7 @@ const ProjectList = () => {
 
     const {data, isSuccess, isLoading} = getApiState(plugin_constants.pluginApiServiceConfigs.getAllLinkedProjectsList.apiServiceName);
     const projectsList = data as ProjectDetails[] ?? [];
-    const sortedProjectList = useMemo(() => [...projectsList].sort(sortProjectList), [projectsList]);
+    const sortedProjectList = useMemo(() => [...projectsList].sort(sortProjectList), [projectsList]); // TODO: Look for best optimisation method here
 
     return (
         <>

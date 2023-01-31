84596b604efeb46a5c833cd6b19ca167976cdbc4
Abhishek Verma <72438220+avas27JTG@users.noreply.github.com>
2023-01-31 11:13:08 +0530

    [MI-2669]: Added some key-values in plugin.json file and created separate functions for custom error components (#30)
    * Added some key-values in plugin.json file and created separate functions for custom error components
    
    * [MI-2669]: Added block lines
    
    ---------
    
    Co-authored-by: Abhishek Verma <abhishek.verma@brightscout.com>

--- webapp/src/utils/errorHandling.ts
+++ webapp/src/utils/errorHandling.ts
@@ -1,5 +1,51 @@
 import pluginConstants from 'pluginConstants';
 
+const getSubscribeModalCustomError = (errorState?: ApiErrorResponse) => {
+    if (errorState?.status === 400 && errorState?.data.Error === pluginConstants.messages.error.subscriptionAlreadyExists) {
+        return pluginConstants.messages.error.subscriptionAlreadyExists;
+    }
+
+    if (errorState?.status === 403 && errorState?.data.Error.includes(pluginConstants.messages.error.accessDenied)) {
+        return pluginConstants.messages.error.adminAccessError;
+    }
+
+    if (errorState?.status === 500 && errorState?.data.Error.includes(pluginConstants.messages.error.failedToGetSubscriptions)) {
+        return pluginConstants.messages.error.failedToGetSubscriptions;
+    }
+
+    return null;
+};
+
+const getLinkProjectModalCustomError = (errorState?: ApiErrorResponse) => {
+    if (errorState?.status === 404 || errorState?.status === 401) {
+        return pluginConstants.messages.error.notAccessibleError;
+    }
+
+    if (errorState?.status === 500 && errorState?.data.Error.includes(pluginConstants.messages.error.errorExpectedForOAuthNotEnabled)) {
+        return pluginConstants.messages.error.errorMessageOAuthNotEnabled;
+    }
+
+    return null;
+};
+
+const confirmationModalCustomError = (errorState?: ApiErrorResponse) => {
+    if (errorState?.status === 403 && errorState?.data.Error.includes(pluginConstants.messages.error.accessDenied)) {
+        return pluginConstants.messages.error.adminAccessError;
+    }
+
+    if (errorState?.status === 404 && errorState?.data.Error.includes(pluginConstants.messages.error.subscriptionNotFound)) {
+        return pluginConstants.messages.error.subscriptionNotFound;
+    }
+
+    return null;
+};
+
+const customErrorAndComponentMap: Partial<Record<ErrorComponents, (errorState?: ApiErrorResponse) => string | null>> = {
+    SubscribeModal: (errorState?: ApiErrorResponse) => getSubscribeModalCustomError(errorState),
+    LinkProjectModal: (errorState?: ApiErrorResponse) => getLinkProjectModalCustomError(errorState),
+    ConfirmationModal: (errorState?: ApiErrorResponse) => confirmationModalCustomError(errorState),
+};
+
 const getErrorMessage = (
     isError: boolean,
     component: ErrorComponents,
@@ -9,40 +55,8 @@ const getErrorMessage = (
         return '';
     }
 
-    switch (component) {
-    case 'SubscribeModal': // Create subscription modal
-        if (errorState?.status === 400 && errorState?.data.Error === pluginConstants.messages.error.subscriptionAlreadyExists) {
-            return pluginConstants.messages.error.subscriptionAlreadyExists;
-        }
-        if (errorState?.status === 403 && errorState?.data.Error.includes(pluginConstants.messages.error.accessDenied)) {
-            return pluginConstants.messages.error.adminAccessError;
-        }
-        if (errorState?.status === 500 && errorState?.data.Error.includes(pluginConstants.messages.error.failedToGetSubscriptions)) {
-            return pluginConstants.messages.error.failedToGetSubscriptions;
-        }
-        return errorState?.data.Error ?? pluginConstants.messages.error.generic;
-
-    case 'LinkProjectModal':
-        if (errorState?.status === 404 || errorState?.status === 401) {
-            return pluginConstants.messages.error.notAccessibleError;
-        }
-        if (errorState?.status === 500 && errorState?.data.Error.includes(pluginConstants.messages.error.errorExpectedForOAuthNotEnabled)) {
-            return pluginConstants.messages.error.errorMessageOAuthNotEnabled;
-        }
-        return errorState?.data.Error ?? pluginConstants.messages.error.generic;
-
-    case 'ConfirmationModal':
-        if (errorState?.status === 403 && errorState?.data.Error.includes(pluginConstants.messages.error.accessDenied)) {
-            return pluginConstants.messages.error.adminAccessError;
-        }
-        if (errorState?.status === 404 && errorState?.data.Error.includes(pluginConstants.messages.error.subscriptionNotFound)) {
-            return pluginConstants.messages.error.subscriptionNotFound;
-        }
-        return pluginConstants.messages.error.generic;
-
-    default:
-        return errorState?.data.Error ?? pluginConstants.messages.error.generic;
-    }
+    // Return custom or API error for different components
+    return customErrorAndComponentMap[component]?.(errorState) ?? errorState?.data.Error ?? pluginConstants.messages.error.generic;
 };
 
 export default getErrorMessage;
